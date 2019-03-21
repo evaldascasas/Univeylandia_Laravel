a83fb9867c8c065c98858723e1aa316028725ddb
Evaldas <evaldas_casas@hotmail.com>
2019-03-21 19:08:52 +0100

    Canvi en el seed de Noticies (falten imatges)

--- database/seeds/NoticiesSeeder.php
+++ database/seeds/NoticiesSeeder.php
@@ -16,15 +16,15 @@ class NoticiesSeeder extends Seeder
             'descripcio' => 'El parc tancarà el día 20/03/2019 per la exposició de la presentació del sprint 4',
             'id_usuari' => 10,
             'categoria' => 1,
-            'path_img' => '/storage/noticies/test.png',
+            'path_img' => '/storage/noticies/tancat.jpg',
         ]);
 
         DB::table('noticies')->insert([
-            'titol' => 'Nova atracció! Dragon khan',
-            'descripcio' => 'Dragon Khan es una montaña rusa de PortAventura Park. Es la segunda atracción más famosa del parque después de la montaña rusa Shambhala. El parque está ubicado en Salou y Vilaseca, municipios de Tarragona, Cataluña, España. Fue inaugurada el 1 de mayo de 1995 por el presidente de la Generalidad de Cataluña Jordi Pujol, acompañado por el ministro de Comercio y Turismo de España Javier Gómez Navarro',
+            'titol' => 'Nova atracció!',
+            'descripcio' => 'El dia 23/05/2019 inaugurarem una nova atracció anomenada Furious Agramunt. Aquesta atracció posarà a prova als més valents, passant de 0 a 150 km/h en tan sols 2 segons. L\'atracció té un recorregut que llista gairebé tocant el terra.No t\'ho perdis!',
             'id_usuari' => 10,
             'categoria' => 2,
-            'path_img' => '/storage/noticies/test.png',
+            'path_img' => '/storage/noticies/furiousAgramunt.jpg',
         ]);
 
         DB::table('noticies')->insert([
@@ -32,7 +32,7 @@ class NoticiesSeeder extends Seeder
             'descripcio' => 'La decoració de nadal estarà disponible a partir del 20/03/2019, toca celebrar la finalització del sprint! :)',
             'id_usuari' => 10,
             'categoria' => 1,
-            'path_img' => '/storage/noticies/test.png',
+            'path_img' => '/storage/noticies/nadal.jpg',
         ]);
     }
 }
