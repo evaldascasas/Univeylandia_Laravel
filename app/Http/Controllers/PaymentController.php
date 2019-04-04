583a59a6fa8459c51fe172059d4277dafcdb7628
Ferran <ferrancliment2@iesmontsia.org>
2019-04-04 19:51:44 +0200

    Equip Directiu v1.0
    Finish (falta foto JSON de jose febrer)

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -56,34 +56,39 @@ class PaymentController extends Controller
     }
     public function payWithpaypal(Request $request)
     {
-        $payer = new Payer();
+        $payer = new Payer();                                                                         //Crear un nou usuari per el  pagament
         $payer->setPaymentMethod('paypal');
-        $item_1 = new Item();
-        $item_1->setName('Item 1') /** item name **/
-            ->setCurrency('USD')
+
+        $item_1 = new Item();                                                                           //Crear un nou item amb els seus atributs
+        $item_1->setName('Item 1') 
+            ->setCurrency('EUR')
             ->setQuantity(1)
-            ->setPrice($request->get('amount')); /** unit price **/
-        $item_list = new ItemList();
-        $item_list->setItems(array($item_1));
+            ->setPrice($request->get('amount')); 
+            
+        $item_list = new ItemList();    
+        $item_list->setItems(array($item_1));                                                           //Afegir-ho a la llista d'items
         $amount = new Amount();
-        $amount->setCurrency('USD')
+        $amount->setCurrency('EUR')                                                                     //Amontonar els items
             ->setTotal($request->get('amount'));
-        $transaction = new Transaction();
+
+        $transaction = new Transaction();                                                                //Crear una nova transaction
         $transaction->setAmount($amount)
             ->setItemList($item_list)
             ->setDescription('Your transaction description');
+            
         $redirect_urls = new RedirectUrls();
-        $redirect_urls->setReturnUrl(URL::to('status')) /** Specify return URL **/
+        $redirect_urls->setReturnUrl(URL::to('status'))                                                 //Crear una nova URL i mostrar el estat
             ->setCancelUrl(URL::to('status'));
-        $payment = new Payment();
+
+        $payment = new Payment();                                                                       //Crear un nou pagament i assignar el usuari, i la transaction
         $payment->setIntent('Sale')
             ->setPayer($payer)
             ->setRedirectUrls($redirect_urls)
             ->setTransactions(array($transaction));
-        /** dd($payment->create($this->_api_context));exit; **/
-        try {
+
+        try {                                                                                           //Afegir el pagament al context de la API
             $payment->create($this->_api_context);
-        } catch (\PayPal\Exception\PPConnectionException $ex) {
+        } catch (\PayPal\Exception\PPConnectionException $ex) {                                         //Errors de temps de conexio i errors desconeguts
             if (\Config::get('app.debug')) {
                 \Session::put('error', 'Conexio fora de temps');
                 return Redirect::to('/compra');
@@ -98,38 +103,36 @@ class PaymentController extends Controller
                 break;
             }
         }
-        /** add payment ID to session **/
-        Session::put('paypal_payment_id', $payment->getId());
+
+        Session::put('paypal_payment_id', $payment->getId());                                           //Afegir el pagament al ID d'usuari
         if (isset($redirect_url)) {
             /** redirect to paypal **/
             return Redirect::away($redirect_url);
         }
-        \Session::put('error', 'Error desconegut');
+        \Session::put('error', 'Error desconegut');                                                     //Error desconegut 
         return Redirect::to('/compra');
     }
-    public function getPaymentStatus()
+    public function getPaymentStatus()                                                                  //Actio del pagament
     {
-        /** Get the payment ID before session clear **/
-        $payment_id = Session::get('paypal_payment_id');
-        /** clear the session payment ID **/
-        Session::forget('paypal_payment_id');
+        $payment_id = Session::get('paypal_payment_id');                                                //ID del pagment a la sessio
+        Session::forget('paypal_payment_id');                                                           //Oblidar el ID
         if (empty(Input::get('PayerID')) || empty(Input::get('token'))) {
-            \Session::put('error', 'Error amb el pagament');
+            \Session::put('error', 'Error amb el pagament');                                            //Error de pagament
             return Redirect::to('/compra');
         }
         $payment = Payment::get($payment_id, $this->_api_context);
         $execution = new PaymentExecution();
-        $execution->setPayerId(Input::get('PayerID'));
+        $execution->setPayerId(Input::get('PayerID'));                                                  //Executar el pagament
         /**Execute the payment **/
         $result = $payment->execute($execution, $this->_api_context);
-        if ($result->getState() == 'approved') {
+        if ($result->getState() == 'approved') {                                                        //Si esta aprovat executar la funcio de compra_finalitzada();
 
             $prova = (new HomeController)->compra_finalitzada();
             
             \Session::put('success', 'Compra realitzada correctament');
             return Redirect::to('/cistella');
         }
-        \Session::put('error', 'Error amb el pagament');
+        \Session::put('error', 'Error amb el pagament');                                                   //Error de pagament
         return Redirect::to('/compra');
     }
 }
