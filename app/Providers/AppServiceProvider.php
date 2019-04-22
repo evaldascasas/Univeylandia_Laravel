e2d49475874bd4bcdcf3fe3dcecf499199b36a7f
Ferran <ferrancliment2@iesmontsia.org>
2019-04-22 17:06:49 +0200

    LARAVEL DUSK

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->isLocal()) {
             $this->app->register(TelescopeServiceProvider::class);
+            $this->app->register(\Laravel\Dusk\DuskServiceProvider::class);
         }
     }
 
