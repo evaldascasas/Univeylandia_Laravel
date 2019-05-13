6bfa75585b5d301f4331d4a858a17497227c48c7
Evaldas <evaldas_casas@hotmail.com>
2019-05-13 22:33:56 +0200

    canvi Query Assignacio Empleat Atraccio

--- app/AssignacioAtraccion.php
+++ app/AssignacioAtraccion.php
@@ -21,16 +21,20 @@ class AssignacioAtraccion extends Model
     public static function AssignacioFiltre($data_inici, $data_fi, $id_rol)
     {
         $empleats = DB::select(
-            'SELECT * FROM users WHERE users.id_rol = ' . $id_rol . ' AND users.id NOT IN (
+            "SELECT * FROM users WHERE users.id_rol = $id_rol AND users.id NOT IN (
           SELECT
              assign_emp_atraccions.id_empleat
           FROM
              assign_emp_atraccions
           WHERE
-             ( assign_emp_atraccions.data_inici <= "' . $data_inici . '" AND assign_emp_atraccions.data_fi >= "' . $data_fi . '")
+             ('$data_inici' BETWEEN assign_emp_atraccions.data_inici AND date_sub(assign_emp_atraccions.data_fi, INTERVAL +1 day))
              OR
-             ( assign_emp_atraccions.data_inici >= "' . $data_inici . '" AND assign_emp_atraccions.data_fi <= "' . $data_fi . '")
-           )'
+             ('$data_fi' BETWEEN date_sub(assign_emp_atraccions.data_inici, INTERVAL -1 day) AND assign_emp_atraccions.data_fi)
+             OR
+             ( assign_emp_atraccions.data_inici <= '$data_inici' AND assign_emp_atraccions.data_fi >= '$data_fi')
+             OR
+             ( assign_emp_atraccions.data_inici >= '$data_inici' AND assign_emp_atraccions.data_fi <= '$data_fi')
+           )"
         );
 
         return $empleats;
